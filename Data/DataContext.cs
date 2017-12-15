d1f6bd07b3728f5389b9abaed64c20e8e72e66e2
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-12-15 01:33:39 +0100

    updated core reference, added logging into data context and implemented database config file handling

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,11 +1,55 @@
-﻿using ch.wuerth.tobias.mux.Data.models;
+﻿using System;
+using System.IO;
+using ch.wuerth.tobias.mux.Core.exceptions;
+using ch.wuerth.tobias.mux.Core.io;
+using ch.wuerth.tobias.mux.Core.logging;
+using ch.wuerth.tobias.mux.Data.models;
+using global::ch.wuerth.tobias.mux.Core.global;
 using Microsoft.EntityFrameworkCore;
 
 namespace ch.wuerth.tobias.mux.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        private readonly DatabaseConfig _config;
+        private readonly LoggerBundle _logger;
+
+        public DataContext(DbContextOptions<DataContext> options, LoggerBundle logger = null) : base(options)
+        {
+            _logger = logger;
+
+            _logger?.Information?.Log("Initializing data context...");
+
+            if (!File.Exists(DatabaseSettingsFilePath))
+            {
+                _logger?.Information?.Log($"File '{DatabaseSettingsFilePath}' not found. Trying to create it...");
+                Boolean save = FileInterface.Save(new DatabaseConfig(), DatabaseSettingsFilePath, true, _logger);
+                if (!save)
+                {
+                    throw new ProcessAbortedException();
+                }
+
+                _logger?.Information?.Log($"File '{DatabaseSettingsFilePath}' successfully created.");
+                _logger?.Information?.Log("Please edit the file and adjust as needed before restarting the application.");
+                throw new ProcessAbortedException();
+            }
+
+            _logger?.Information?.Log($"Loading database config from '{DatabaseSettingsFilePath}'...");
+
+            (DatabaseConfig output, Boolean success) res = FileInterface.Read<DatabaseConfig>(DatabaseSettingsFilePath);
+            if (!res.success)
+            {
+                throw new ProcessAbortedException();
+            }
+
+            _config = res.output;
+            _logger?.Information?.Log("Context initialized.");
+        }
+
+        private static String DatabaseSettingsFilePath
+        {
+            get { return Path.Combine(Location.ApplicationDataDirectoryPath, @"\mux_config_database.json"); }
+        }
 
         public virtual DbSet<User> SetUsers { get; set; }
         public virtual DbSet<Track> SetTracks { get; set; }
@@ -24,12 +68,17 @@ namespace ch.wuerth.tobias.mux.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // todo read connectionstring from central settings file
-            optionsBuilder.UseSqlServer(@"YOUR_CONNECTIONSTRING");
+            _logger?.Information?.Log("Configuring context...");
+
+            optionsBuilder.UseSqlServer(_config.ConnectionString);
+
+            _logger?.Information?.Log("Context configured.");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            _logger?.Information?.Log("Creating database model...");
+
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<User>().HasIndex(x => x.Username).IsUnique();
@@ -52,6 +101,8 @@ namespace ch.wuerth.tobias.mux.Data
             modelBuilder.Entity<MusicBrainzTextRepresentation>().HasIndex(x => x.UniqueHash).IsUnique();
             modelBuilder.Entity<MusicBrainzReleaseEvent>().HasIndex(x => x.UniqueHash).IsUnique();
             modelBuilder.Entity<MusicBrainzIsoCode>().HasIndex(x => x.Code).IsUnique();
+
+            _logger?.Information?.Log("Model created.");
         }
     }
 }
---
+++ Data/DatabaseConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch.wuerth.tobias.mux.Data
+{
+    public class DatabaseConfig
+    {
+        public String ConnectionString { get; set; }
+    }
+}

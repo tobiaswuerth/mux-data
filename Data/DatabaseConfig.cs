ddc945b7074c9165badf101805b92b47c6d6f454
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-12-19 23:22:17 +0100

    removed notsupported connection string attribute

--- Data/DatabaseConfig.cs
+++ Data/DatabaseConfig.cs
@@ -11,7 +11,7 @@ namespace ch.wuerth.tobias.mux.Data
         public DatabaseConfig()
         {
             ConnectionString =
-                "Data Source=localhost;Initial Catalog=Mux;Integrated Security=True;Persist Security Info=False;User ID=;Password=;Asynchronous Processing=True;MultipleActiveResultSets=True;Connect Timeout=5;Encrypt=True;TrustServerCertificate=True;Application Name=Mux;ConnectRetryCount=3;ConnectRetryInterval=5";
+                "Data Source=localhost;Initial Catalog=Mux;Integrated Security=True;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=True;Connect Timeout=5;Encrypt=True;TrustServerCertificate=True;Application Name=Mux;ConnectRetryCount=3;ConnectRetryInterval=5";
         }
     }
 }
